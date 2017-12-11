f0d122f97992ad1d26193f56ea7c7011aa1ac7c4
Timothy Brathwaite <timothyb0912@gmail.com>
2017-12-10 23:15:25 -0800

    Allowing for use of bootstrap class from import pylogit.

--- pylogit/__init__.py
+++ pylogit/__init__.py
@@ -8,5 +8,6 @@ Created on Mon Mar 14 15:33:07 2016
 from __future__ import absolute_import
 
 from .pylogit import create_choice_model
+from .bootstrap import Boot
 from .choice_tools import convert_wide_to_long
 from .choice_tools import convert_long_to_wide
